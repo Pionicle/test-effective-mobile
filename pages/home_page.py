55860c3750bb653d688583692f293acd5081f6c4
Ivan Stolyarov <stolyarovioann@gmail.com>
2025-01-24 12:13:58 +0300

    вынес локаторы

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,18 @@
 from playwright.sync_api import Page, Locator
 
 
+class HomePageLocators:
+    """Класс для хранения локаторов."""
+
+    MAIN_LINK = "div.tn-atom a[href='#main']"
+    ABOUT_LINK = "a.tn-atom[href='#about']"
+    MOREINFO_LINK = "a.tn-atom[href='#moreinfo']"
+    CASES_LINK = "a.tn-atom[href='#cases']"
+    REVIEWS_LINK = "a.tn-atom[href='#Reviews']"
+    CONTACTS_LINK = "a.tn-atom[href='#contacts']"
+    SPECIALISTS_LINK = "a.tn-atom[href='#specialists']"
+
+
 class HomePage:
     def __init__(self, page: Page):
         self.page = page
@@ -8,37 +20,37 @@ class HomePage:
     @property
     def main_link(self):
         """Локатор для главной раздела."""
-        return self.page.locator("div.tn-atom a[href='#main']").all()
+        return self.page.locator(HomePageLocators.MAIN_LINK).all()
 
     @property
     def about_link(self):
         """Локатор для раздела 'О нас'."""
-        return self.page.locator("a.tn-atom[href='#about']").all()
+        return self.page.locator(HomePageLocators.ABOUT_LINK).all()
 
     @property
     def moreinfo_link(self):
         """Локатор для раздела 'Услуги'."""
-        return self.page.locator("a.tn-atom[href='#moreinfo']").all()
+        return self.page.locator(HomePageLocators.MOREINFO_LINK).all()
 
     @property
     def cases_link(self):
         """Локатор для раздела 'Проекты'."""
-        return self.page.locator("a.tn-atom[href='#cases']").all()
+        return self.page.locator(HomePageLocators.CASES_LINK).all()
 
     @property
     def reviews_link(self):
         """Локатор для раздела 'Отзывы'."""
-        return self.page.locator("a.tn-atom[href='#Reviews']").all()
+        return self.page.locator(HomePageLocators.REVIEWS_LINK).all()
 
     @property
     def contacts_link(self):
         """Локатор для раздела 'Контакты'."""
-        return self.page.locator("a.tn-atom[href='#contacts']").all()
+        return self.page.locator(HomePageLocators.CONTACTS_LINK).all()
 
     @property
     def specialists_link(self):
         """Локатор для раздела 'Выбрать специалиста'."""
-        return self.page.locator("a.tn-atom[href='#specialists']").all()
+        return self.page.locator(HomePageLocators.SPECIALISTS_LINK).all()
 
     @property
     def url(self) -> str:
